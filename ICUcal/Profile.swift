46983009e932132663a0fbde3b70cd0c7a792636
mingyanisa <mingignim@hotmail.com>
2017-08-18 23:58:30 +0700

    add bug delete patient

--- ICUcal/Profile.swift
+++ ICUcal/Profile.swift
@@ -12,6 +12,27 @@ class ProfileViewController: UIViewController {
     
     var patient: Patient?
     
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+    @IBOutlet weak var genderLabel: UILabel!
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var sicknessLabel: UILabel!
+    @IBOutlet weak var allergyLabel: UILabel!
+    @IBOutlet weak var noteLabel: UILabel!
+    
+    override func viewDidLoad() {
+        if let data = patient {
+            nameLabel.text = data.name
+            ageLabel.text = String(describing:data.age)
+            genderLabel.text = data.sex
+            heightLabel.text = String(describing: data.height)
+            weightLabel.text = String(describing: data.weight)
+            sicknessLabel.text = data.sickness
+            allergyLabel.text = data.allergy
+            noteLabel.text = data.note
+        }
+    }
     @IBAction func doneBtnPressed(_ sender: UIBarButtonItem) {
         self.dismiss(animated: true, completion: nil)
     }
@@ -27,5 +48,7 @@ class ProfileViewController: UIViewController {
                 break
             }
         }
+        
+        
     }
 }
